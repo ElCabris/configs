fe32e81f5e78a0b31568eabb26524a1baf5761d5
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-08-19 18:52:20 -0500

    add new lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- Tema
+vim.cmd.colorscheme("vim")
+
 -- Configuración para netrw
 vim.opt.compatible = false -- set nocp
 vim.cmd("filetype plugin on")
@@ -31,7 +34,7 @@ if not vim.loop.fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -50,30 +53,148 @@ require("lazy").setup({
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/cmp-buffer" },
     { "hrsh7th/cmp-path" },
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+        require("nvim-autopairs").setup({ check_ts = true })
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end,
+    },
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
 })
 
--- Mapeos de teclas para LSP
-vim.api.nvim_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gr', '<Cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>f', '<Cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+-- Función on_attach
+local function on_attach(client, bufnr)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
 
--- Configuración de LSP para C y C++ usando clangd
+  local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+  if filetype == "python" then
+    vim.keymap.set("n", "<leader>f", function()
+      vim.cmd("!black %")
+      vim.cmd("edit!")
+    end, opts)
+  elseif client.supports_method("textDocument/formatting") then
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end
+end
+
 local lspconfig = require('lspconfig')
+
+-- Configuración de LSPs
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+lspconfig.sqls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+lspconfig.bashls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+lspconfig.lua_ls.setup({
+  cmd = { vim.fn.expand("~/tools/lua-language-server/bin/lua-language-server") },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = { enable = false },
+    },
+  },
+})
+
+
+lspconfig.jdtls.setup {
+  cmd = { 'jdtls', "-data", vim.fn.expand("~/cache/jdtls-workspace"), "--jvm-arg=--enable-native-access=ALL-UNNAMED" },
+  root_dir = lspconfig.util.root_pattern('pom.xml', 'gradle.build', '.git'),
+  on_attach = on_attach,
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      contentProvider = { preferred = 'fernflower' },
+
+      imports = {
+        gradle = {
+          wrapper = {
+            checksums = {
+              {
+                sha256 = "7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172",
+                allowed = true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+lspconfig.gopls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        unreachable = true,
+      },
+      staticcheck = true,
+    },
+  },
+})
+
+lspconfig.html.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
+
+lspconfig.cssls.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
 
 lspconfig.clangd.setup({
   capabilities = capabilities,
   cmd = { "clangd" },
   filetype = { "c", "cpp", "objc", "objcpp" },
-  root_dir = lspconfig.util.root_pattern(".git", "compile_commands.json", "CMakeLists.txt")
+  root_dir = lspconfig.util.root_pattern(".git", "compile_commands.json", "CMakeLists.txt"),
+  on_attach = on_attach,
 })
 
 lspconfig.pyright.setup({
   capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+lspconfig.r_language_server.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+lspconfig.ts_ls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
 -- Configuración nvim-cmp
